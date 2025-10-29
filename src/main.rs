17be9251abf72d5e7168dc5c476738538d813063
Schmarni <marnistromer@gmail.com>
2025-10-29 16:18:41 +0100

    feat: initial commit, some trigangle function test stuff
    Signed-off-by: Schmarni <marnistromer@gmail.com>

---
+++ src/main.rs
@@ -0,0 +1,111 @@
+use std::sync::Mutex;
+
+use glam::{Mat4, Vec3};
+use stardust_xr_fusion::{
+    client::Client,
+    drawable::{Line, LinePoint, Lines, LinesAspect},
+    fields::{Field, Shape},
+    input::InputHandler,
+    node::NodeResult,
+    root::{RootAspect, RootEvent},
+    spatial::Transform,
+    values::color::rgba_linear,
+};
+use stardust_xr_molecules::{
+    input_action::{InputQueueable, MultiAction},
+    lines::{self, LineExt},
+};
+
+#[tokio::main]
+async fn main() -> NodeResult<()> {
+    let client = Client::connect().await.unwrap();
+    let event_loop = client.async_event_loop();
+    let client = event_loop.client_handle.clone();
+    let field = Field::create(client.get_root(), Transform::none(), Shape::Sphere(1.0))?;
+    let mut input_handler =
+        InputHandler::create(client.get_root(), Transform::none(), &field)?.queue()?;
+    let mut action = MultiAction::default();
+    let lines = Lines::create(client.get_root(), Transform::none(), &[])?;
+
+    loop {
+        event_loop.get_event_handle().wait().await;
+        let Some(event) = client.get_root().recv_root_event() else {
+            continue;
+        };
+        match event {
+            RootEvent::Ping { response } => response.send_ok(()),
+            RootEvent::Frame { info: _ } => {}
+            RootEvent::SaveState { response: _ } => {
+                break;
+            }
+        }
+
+        let points: Mutex<Vec<[Vec3; 3]>> = Mutex::new(Vec::new());
+        input_handler.handle_events();
+        action.update(
+            &input_handler,
+            |_| true,
+            |v| match &v.input {
+                stardust_xr_fusion::input::InputDataType::Pointer(_) => false,
+                stardust_xr_fusion::input::InputDataType::Hand(hand) => {
+                    let mut points = points.lock().unwrap();
+                    let mut p = [
+                        hand.thumb.tip.position.into(),
+                        hand.index.tip.position.into(),
+                        hand.middle.tip.position.into(),
+                    ];
+                    if !hand.right {
+                        p.reverse();
+                    }
+                    points.push(p);
+                    false
+                }
+                stardust_xr_fusion::input::InputDataType::Tip(_) => false,
+            },
+        );
+        let mut lines_data = Vec::new();
+        for points in points.lock().unwrap().clone() {
+            let [a, b, c] = points;
+            let ab = a.distance_squared(b);
+            let bc = b.distance_squared(c);
+            let ca = c.distance_squared(a);
+            let point_a = ((bc * a) + (ca * b) + (ab * c)) / (ab + bc + ca);
+            let a_dist = a.distance_squared(point_a);
+            let b_dist = b.distance_squared(point_a);
+            let c_dist = c.distance_squared(point_a);
+            let point = ((a_dist * a) + (b_dist * b) + (c_dist * c)) / (a_dist + b_dist + c_dist);
+            let point = point.lerp(point_a, 0.5);
+            lines_data.push(Line {
+                points: points
+                    .into_iter()
+                    .map(|p| LinePoint {
+                        point: p.into(),
+                        thickness: 0.001,
+                        color: rgba_linear!(1.0, 0.0, 1.0, 1.0),
+                    })
+                    .collect(),
+                cyclic: true,
+            });
+            let ab = b - a;
+            let ac = c - a;
+            let normal = ab.cross(ac).normalize();
+            lines_data.push(Line {
+                points: vec![
+                    LinePoint {
+                        point: point.into(),
+                        thickness: 0.001,
+                        color: rgba_linear!(0.0, 1.0, 0.0, 1.0),
+                    },
+                    LinePoint {
+                        point: (point + (normal * 0.01)).into(),
+                        thickness: 0.001,
+                        color: rgba_linear!(0.0, 1.0, 0.0, 1.0),
+                    },
+                ],
+                cyclic: false,
+            });
+        }
+        lines.set_lines(&lines_data)?;
+    }
+    Ok(())
+}
