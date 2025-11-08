71211ccffabc3c3ab5743831955117ab9d9294c3
Schmarni <marnistromer@gmail.com>
2025-11-08 21:13:41 +0100

    feat: make the movement feel a bit nicer
    Signed-off-by: Schmarni <marnistromer@gmail.com>

--- src/mover.rs
+++ src/mover.rs
@@ -1,16 +1,15 @@
-use glam::{Quat, Vec3, Vec3A};
+use glam::{FloatExt, Quat, Vec3, Vec3A};
 use stardust_xr_fusion::{
     node::NodeResult,
     spatial::{Spatial, SpatialAspect, SpatialRef, SpatialRefAspect, Transform},
 };
-use tracing::info;
 
 use crate::selection::CapturedSelection;
 
 pub struct Mover {
     selection: CapturedSelection,
     target: Spatial,
-    _input: SpatialRef,
+    input: SpatialRef,
     // m/s
     // selection_velocity: Vec3A,
     // rotation axis scaled by radians/s
@@ -31,19 +30,38 @@ impl Mover {
         Ok(Self {
             selection,
             target,
-            _input: input_spatial,
+            input: input_spatial,
         })
     }
     pub async fn update(&mut self) {
         let sel = self.selection.spatial();
-        let transform = sel.get_transform(&self.target).await.unwrap();
-        let pre_translation = transform.translation.map(Vec3A::from).unwrap_or_default();
-        let pre_rotation = transform.rotation.map(Quat::from).unwrap_or_default();
+        let sel_transform = sel.get_transform(&self.input).await.unwrap();
+        let target_transform = self.target.get_transform(&self.input).await.unwrap();
+        let sel_translation = sel_transform
+            .translation
+            .map(Vec3A::from)
+            .unwrap_or_default();
+        let sel_rotation = sel_transform.rotation.map(Quat::from).unwrap_or_default();
+        let target_translation = target_transform
+            .translation
+            .map(Vec3A::from)
+            .unwrap_or_default();
+        let target_rotation = target_transform
+            .rotation
+            .map(Quat::from)
+            .unwrap_or_default();
         let lerp_factor = 0.95;
-        let translation = Vec3A::ZERO.lerp(pre_translation, lerp_factor);
-        let rotation = Quat::IDENTITY.lerp(pre_rotation, lerp_factor);
+        let sel_len = sel_translation.length();
+        let target_len = target_translation.length();
+        let sel_quat = Quat::from_rotation_arc(Vec3::NEG_Z, sel_translation.normalize().into());
+        let target_quat = Quat::from_rotation_arc(Vec3::NEG_Z, target_translation.normalize().into());
+        let quat = target_quat.slerp(sel_quat, lerp_factor);
+        let len = target_len.lerp(sel_len, lerp_factor);
+        let translation = (quat * Vec3::NEG_Z) * len;
+        // let translation = target_translation.lerp(sel_translation, lerp_factor);
+        let rotation = target_rotation.slerp(sel_rotation, lerp_factor);
         sel.set_relative_transform(
-            &self.target,
+            &self.input,
             Transform::from_translation_rotation_scale(translation, rotation, Vec3::ONE),
         )
         .unwrap();
